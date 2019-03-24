8b0bcf85fc3ec93e52fe22f5a4330e2fb1ead8ec
royjoys <soumik182006@gmail.com>
2019-03-25 00:18:41 +0530

    Initial commit

---
+++ src/main/java/com/webcrawler/service/WebCrawlerService.java
@@ -0,0 +1,22 @@
+package com.webcrawler.service;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+import com.webcrawler.exception.WebCrawlerException;
+import com.webcrawler.model.Internet;
+import com.webcrawler.model.Output;
+import com.webcrawler.model.Page;
+
+public interface WebCrawlerService {
+
+	Internet readObjectFromJSON(File f) throws WebCrawlerException;
+
+	Map<String, List<String>> formMapFromList(List<Page> pages);
+
+	Output getOutputValues(Map<String, List<String>> input);
+
+	String formJSON(Output output);
+
+}
